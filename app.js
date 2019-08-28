57d8c7fcd36c4a1d4671a7693704315730415abe
dhuang612 <dhuang@madisoncollege.edu>
2019-08-28 13:30:29 -0400

    continuing to work on projects

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const getNotes = require('./notes');
+const chalk = require('chalk');
 const printMsg = getNotes();
-console.log(printMsg);
+
+const command = process.argv[2];
+if (command === 'add') {
+  console.log('note added!');
+} else if (command === 'remove') {
+  console.log('note removed!');
+} else if (command === 'Dan') {
+  console.log('Hi Dan!');
+}
+console.log(process.argv);
